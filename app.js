e105e156ebc415a7367b5acd8927714bca57ea2e
David Souza <david.jsouzaa@gmail.com>
2019-12-07 14:47:45 -0300

    payment credit card

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var mercadopago = require('mercadopago');
+mercadopago.configure({
+    sandbox: true,
+    access_token: 'TEST-249219817450644-111300-1ac6fc62fb47adf917a9b3da9bb907c8-141751821'
+});
 var port = process.env.PORT || 3000
 
 var app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded());
 
 app.use(express.static('assets'));
  
@@ -20,7 +27,33 @@ app.get('/detail', function (req, res) {
 });
 
 app.post('/pay', async function (req, res) {
-    
+    console.log(req.body);
+    const {token } = req.body;
+    const {amount } = req.body;
+    const {installments } = req.body;
+    const {description } = req.body;
+    const {paymentMethodId } = req.body;
+    const {email } = req.body;
+
+    var payment_data = {
+        transaction_amount: parseInt(amount),
+        token: token,
+        description: description,
+        installments: parseInt(installments),
+        payment_method_id: paymentMethodId,
+        payer: {
+          email: email
+        }
+      };
+
+
+    // Save and posting the payment
+    mercadopago.payment.save(payment_data).then(function (data) {
+        console.log(data);
+        res.send(data);
+    }).catch(function (error) {
+        console.log(error);
+    });
 })
 
 app.listen(port);
