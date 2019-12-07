f92d294e5bd706aa791e9b115f4aca4881c26af6
Edival Martin <edival.martin@gmail.com>
2019-12-07 15:16:54 -0300

    Implementação inicial com o status e o detalhamento do status de aprovação.

--- app.js
+++ app.js
@@ -28,6 +28,32 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
+function getPaymentStatus(status) {
+    let status_pt_br = status;
+    
+    if (status == 'approved') { status_pt_br = 'Aprovado' };
+    if (status == 'rejected') { status_pt_br = 'Rejeitado' };
+    if (status == 'in_process') { status_pt_br = 'Em processamento' };
+
+    return status_pt_br;
+};
+
+function getPaymentStatusDetail(statusDetail) {
+    let descStatus = statusDetail;
+
+    if (statusDetail == 'accredited') {
+        descStatus = 'Pronto, seu pagamento foi aprovado!';
+    }
+    else if (statusDetail == 'pending_contingency') {
+        descStatus = 'Estamos processando o pagamento. Em até 2 dias úteis informaremos por e-mail o resultado.';
+    }
+    else if (statusDetail == 'cc_rejected_bad_filled_security_code') {
+        descStatus = 'Confira o código de segurança.';
+    }
+
+    return descStatus;
+}
+
 app.post('/process_payment', (req, res) => {
     console.log(req.body);
 
@@ -51,11 +77,17 @@ app.post('/process_payment', (req, res) => {
             console.log(data);
 
             let resData = 
-            `<b>Sua compra foi realizada!<b>
+            `<b>Sua compra foi enviada!</b>
+             <br>Confira abaixo o status do pagamento
              <br>
-             Dados da compra:
+             <h1>Status do Pagamento:<b> ${getPaymentStatus(data.body.status)}</b></h1>
+             <p> ${getPaymentStatusDetail(data.body.status_detail)} </p>
+
              <br>
+             JSON Response:
+             <div>
              ${JSON.stringify(data)}
+             </div>
              `;
 
             res.send(resData);
